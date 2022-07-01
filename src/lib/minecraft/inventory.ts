713f35b2dcb033898ecf1477b6d2cf895d37aac3
mat <github@matdoes.dev>
2022-07-01 00:40:33 -0500

    Fix item heads

--- src/lib/minecraft/inventory.ts
+++ src/lib/minecraft/inventory.ts
@@ -104,7 +104,7 @@ let itemUrlCache: Record<string, string> = {}
 setInterval(() => {
     itemUrlCache = {}
 }, 120 * 1000)
-export function itemToUrl(item: Item, pack?: skyblockAssets.MatcherFile, headSize?: number): string {
+export function itemToUrl(item: Item, pack?: skyblockAssets.MatcherFile): string {
     const itemNbt: skyblockAssets.NBT = {
         display: {
             Name: item.display?.name
@@ -129,8 +129,6 @@ export function itemToUrl(item: Item, pack?: skyblockAssets.MatcherFile, headSiz
         })
         if (textureUrl === null) {
             textureUrl = `https://h.matdoes.dev/3d/${item.headTexture}`
-            if (headSize)
-                textureUrl += `/${headSize}`
         }
     } else {
         textureUrl = skyblockAssets.getTextureUrl({
@@ -143,9 +141,9 @@ export function itemToUrl(item: Item, pack?: skyblockAssets.MatcherFile, headSiz
     return textureUrl
 }
 
-export function skyblockItemToUrl(skyblockItem: string | Item, pack?: skyblockAssets.MatcherFile, headSize?: number) {
+export function skyblockItemToUrl(skyblockItem: string | Item, pack?: skyblockAssets.MatcherFile) {
     const item = typeof skyblockItem === 'string' ? skyblockItemNameToItem(skyblockItem) : skyblockItem
-    const itemTextureUrl = itemToUrl(item, pack, headSize)
+    const itemTextureUrl = itemToUrl(item, pack)
     return itemTextureUrl
 }
 
